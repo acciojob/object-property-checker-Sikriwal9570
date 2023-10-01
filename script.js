6e44f10d19018f9939c696462f5e3f4084c22aa3
Sikriwal9570 <128585704+Sikriwal9570@users.noreply.github.com>
2023-10-01 17:06:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
-
 function hasKey(key) {
-  //   write your code here
+  // Check if the key exists in the global object sampleObject
+  return key in sampleObject;
 }
 
-// Do not change the code below
-const key = prompt("Enter Key.");
-alert(hasKey(key));
+// Define the global object sampleObject
+const sampleObject = {
+  red: "#FF0000",
+  green: "#00FF00",
+  white: "#FFFFFF",
+};
+
+// Example usage:
+console.log(hasKey("red"));    // true
+console.log(hasKey("blue"));   // false
+console.log(hasKey("white"));  // true
